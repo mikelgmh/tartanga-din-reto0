c592ff847d6e572b95ae96f8a8240b047702d217
2dam <2dam@TartangaLH.eus>
2020-09-23 14:01:45 +0200

    Primer commit

---
+++ src/dinreto0/com/model/Model.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinreto0.com.model;
+
+/**
+ * Contiene los metodos de modelo para uso en el controlador
+ * @author 2dam
+ */
+public interface Model {
+    /**
+     * Metodo para obtener saludo
+     * @return 
+     */
+    
+    public String getGreeting();
+    
+}
---
+++ src/dinreto0/com/view/View.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinreto0.com.view;
+
+/**
+ * Contiene los metodos de vista para usarlo en el controlador.
+ * @author imanol/mikel
+ */
+public interface View {
+    /**
+     * Metodo para mostrar saludo
+     * @param greeting 
+     */
+    public void showGreeting(String greeting);
+    
+}
