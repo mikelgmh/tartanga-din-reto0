88baaf8b20dbe97077099d7ce83dd28f05378aed
Imanol <Usuario@DESKTOP-KKUNOTO>
2020-09-24 19:50:23 +0200

    Modificado ModelFactory

---
+++ src/dinreto0/com/model/ModelFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dinreto0.com.model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ModelFactory {
+    
+    public Model getModel() {
+        return new ModelImplementation();
+    }
+}
