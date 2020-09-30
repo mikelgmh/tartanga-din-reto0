3badd099a61179c533b94cb122a1e97aafe92748
Imanol <2dam@HZ341338.TartangaLH.eus>
2020-09-30 13:29:39 +0200

    Obtencion de saludo via consulta base de datos

--- src/dinreto0/com/model/MySQLModelImplementation.java
+++ src/dinreto0/com/model/MySQLModelImplementation.java
@@ -7,9 +7,14 @@ package dinreto0.com.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,13 +25,51 @@ public class MySQLModelImplementation implements Model {
     private Connection con;
     private Statement stmt;
     private ResourceBundle rb = ResourceBundle.getBundle("config.config");
-        
     
+    public MySQLModelImplementation() {
+        this.openConnection();
+    }
+     
+    private void openConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", rb.getString("user"));
+            connectionProps.put("password", rb.getString("password"));
+            this.con = DriverManager.getConnection(rb.getString("host"), connectionProps);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(MySQLModelImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void closeConeection() throws SQLException {
+        stmt.close();
+        con.close();
+    }
+    
+    //Acceso a Base de Datos para obtención de saludo.
     private String selectGreeting(){
-        String greeting="";
+        String greeting=null;
         
+        try{
+            String query="SELECT FRASE FROM SALUDO WHERE ID IN(SELECT ID FROM PAIS WHERE IDIOMA = 'CASTELLANO')";
+            
+            this.openConnection();
         
-        
+            stmt = con.createStatement();
+            ResultSet rs=stmt.executeQuery(query);
+            while(rs.next()){
+            greeting=rs.getString("FRASE");           
+            }
+            
+            rs.close();
+            
+            this.closeConeection();
+            
+        }catch(SQLException e){
+            System.out.println("Error en consulta.");
+        }
+   
         return greeting;
     }
     
@@ -37,22 +80,7 @@ public class MySQLModelImplementation implements Model {
         return this.selectGreeting();
     }
     
-    private void openConnection() throws Exception {
-
-        //Revisar user y password
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = rb.getString("host");
-        String name = rb.getString("user");
-        String pass = rb.getString("password");
-        con = DriverManager.getConnection(url, name, pass);
-        System.out.println("Connection created");
-        stmt = con.createStatement();
-    }
-    
-    private void closeConeection() throws SQLException {
-        stmt.close();
-        con.close();
-    }
+  
     
     
     
