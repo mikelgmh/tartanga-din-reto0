559ed44007d3023550cadfefff939aeb9a4f7770
Imanol <2dam@HZ341338.TartangaLH.eus>
2020-10-01 10:09:15 +0200

    Metodo hecho para obtener saludo desde archivo de configuración

--- src/dinreto0/com/model/ModelImplementation.java
+++ src/dinreto0/com/model/ModelImplementation.java
@@ -5,19 +5,23 @@
  */
 package dinreto0.com.model;
 
+import java.util.ResourceBundle;
+
 /**
- *
- * @author 2dam
+ * Obtener saludo desde el archivo de configuración
+ * 
+ * @author Mikel/Imanol
  */
 public class ModelImplementation implements Model {
 
-    /**
-     * Obtiene el saludo.
-     *
-     */
+    private ResourceBundle rb = ResourceBundle.getBundle("config.config");
+    String greeting=null;
+    
+    
     @Override
     public String getGreeting() {
-        return "greeting";
+        greeting=rb.getString("greeting");
+        return greeting;
     }
 
 
